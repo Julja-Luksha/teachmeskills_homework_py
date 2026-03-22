d57b7699dfcba70ab8b56e58d813c1089354206c
Yuliya <julja.jul1995@gmail.com>
2026-03-23 01:09:19 +0300

    task 3 (8)

--- lesson_12/task_3(7-8).py
+++ lesson_12/task_3(7-8).py
@@ -64,3 +64,74 @@ print(i.sort_by_value())
 print(i.sort_by_weight())
 print(i.get_total_weight())
 print(i.get_total_value())
+
+# Задание 8. Класс «Тренажёрный зал»
+# Создай класс Gym.
+# Требования:
+# 1. Атрибуты: название зала, список клиентов (имя, возраст, абонемент активен/не
+# активен).
+# 2. Методы:
+# o add_client(name, age) — добавить клиента.
+# o remove_client(name) — удалить клиента.
+# o activate_membership(name) — активировать абонемент клиента.
+# o deactivate_membership(name) — деактивировать абонемент.
+# o get_active_members() — вернуть список клиентов с активным абонементом.
+# o find_youngest_client() — вернуть самого молодого клиента.
+# o find_oldest_client() — вернуть самого старшего клиента.
+# o average_age() — средний возраст клиентов.
+
+class Gym:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.clients = []
+
+    def add_client(self, name: str, age: int) -> None:
+        client = {
+            "name": name,
+            "age": age,
+            "active": False
+        }
+        self.clients.append(client)
+
+    def remove_client(self, name: str) -> bool:
+        for client in self.clients:
+            if client["name"] == name:
+                self.clients.remove(client)
+            return True
+        return False
+
+    def activate_membership(self, name: str) -> bool:
+        for client in self.clients:
+            if client["name"] == name:
+                client["active"] = True
+                return True
+        return False
+
+    def deactivate_membership(self, name: str) -> bool:
+        for client in self.clients:
+            if client["name"] == name:
+                client["active"] = False
+                return True
+        return False
+
+    def get_active_members(self) -> list:
+        return [client for client in self.clients if client["active"] == True]
+
+    def find_youngest_client(self) -> int:
+        return min(self.clients, key=lambda client: client["age"])
+
+    def find_oldest_client(self) -> int:
+        return max(self.clients, key=lambda client: client["age"])
+
+    def average_age(self) -> float:
+        return sum(client["age"] for client in self.clients) / len(self.clients)
+
+g = Gym("24h")
+g.add_client("Юлия", 31)
+g.add_client("Анна", 20)
+print(g.find_youngest_client())
+print(g.find_oldest_client())
+print(g.average_age())
+print(g.clients)
+g.activate_membership("Юлия")
+print(g.clients)
